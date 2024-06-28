9b7bb7a1b54ccbf36f2d773ed561c20803b5290f
TuanDo <anhtuan7194@gmail.com>
2024-06-28 14:55:34 +0700

    First commit

---
+++ flights/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .models import Flight, Airport, Passenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+# Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {"flights": Flight.objects.all()})
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.filter(id=flight_id).first()
+
+    if not flight:
+        return render(request, "flights/404.html", status=404)
+
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
+
+    return render(
+        request,
+        "flights/flight.html",
+        {"flight": flight, "passengers": passengers, "non_passengers": non_passengers},
+    )
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+
+        # Accessing the flight
+        flight = Flight.objects.get(pk=flight_id)
+
+        # Finding the passenger id from the submitted form data
+        passenger_id = int(request.POST["passenger"])
+
+        # Finding the passenger based on the id
+        passenger = Passenger.objects.get(pk=passenger_id)
+
+        # Add passenger to the flight
+        passenger.flights.add(flight)
+
+        # Redirect user to flight page
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
